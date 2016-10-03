146ca199c45a62d64416aa3dffd8bd463d8dcc74
wind4869 <windv587@gmail.com>
2016-10-03 10:03:50 +0800

    fix: something

--- csv2xls.py
+++ csv2xls.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 
+import os
 import sys
 import pandas
 
--- report.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-
-import os
-import datetime
-
-yesterday = datetime.datetime.strftime(datetime.date.today() - datetime.timedelta(days=1), '%Y%m%d')
-os.popen('scp d67:/home/supertool/haoyouqiang/output/report_%s.csv .' % yesterday)
-os.popen('csv2xls.py report_%s.csv' % yesterday)
-
